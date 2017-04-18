e6acfef98dba4b13fdae6c71def7d5462bd75413
azeemh <azeem.hussein@gmail.com>
2017-04-18 16:40:35 -0400

    updated validations

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -1,5 +1,5 @@
 class Affiliate < ApplicationRecord
   validates :name, presence: true
   validates :affid, presence: true
-  belongs_to :tier
+  belongs_to :tier, optional:true
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,5 +2,5 @@ class Offer < ApplicationRecord
   validates :title, presence: true
   validates :link_to_image, presence: true
   validates :lp_offer_name, presence: true
-  belongs_to :tier
+  belongs_to :tier, optional:true
 end
